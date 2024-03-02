1ec235916f1de67421a0ded144a9d76f89b9a844
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2024-03-02 15:11:56 -0300

    feat: API Key added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'e9ef7085687804a240618d39fe5e8c23'
 };
 
 /*
