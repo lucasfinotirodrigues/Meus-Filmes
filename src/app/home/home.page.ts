d71b05bbb9ff8a369f7fddce60df1af73e05c80b
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2024-03-03 10:22:27 -0300

    feat: inject added at home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, InfiniteScrollCustomEvent } from '@ionic/angular/standalone';
+import { FilmesService } from '../services/filmes.service';
+import { catchError, finalize } from 'rxjs';
+import { MovieResult } from '../services/interface';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +12,50 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  private filmeService = inject(FilmesService);
+  private paginaAtual = 1;
+  private error = null;
+  private isLoading = false;
+  private filmes: MovieResult[] = [];
+  public imageBaseURL = 'http://image.tmdb.org/t/p'
+
+  constructor() {
+    this.carregarFilmes();
+  }
+
+  carregarFilmes(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+
+    if(!event){
+    this.isLoading = true;
+    }
+
+    this.filmeService.getTopFilmes(this.paginaAtual).pipe(
+      finalize(() =>{
+        this.isLoading = false;
+        if(event) {
+          event.target.complete();
+        }
+      }),
+      catchError((err: any) => {
+        console.log(err);
+
+        this.error = err.error.status_message;
+        return []
+      })
+    ).subscribe({
+      next: (res) => {
+        console.log(res);
+
+        this.filmes.push(...res.results);
+        if(event) {
+          event.target.disabled = res.total_pages === this.paginaAtual;
+        }
+      }
+    })
+  }
+
+  carregarMais(event: InfiniteScrollCustomEvent) {
+
+  }
 }
