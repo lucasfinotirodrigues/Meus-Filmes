0376cea1711ee147c8bc044bc2457a888efa3e07
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2024-03-16 14:05:54 -0300

    build: moved API key configuration into service

--- src/app/services/filmes.service.ts
+++ src/app/services/filmes.service.ts
@@ -1,26 +1,24 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 import { ApiResult, MovieResult } from './interface';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = environment.apiKey;
+const API_KEY = 'e9ef7085687804a240618d39fe5e8c23'
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmesService {
 
-  private http = inject(HttpClient)
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTopFilmes(page = 1): Observable<ApiResult>{
-    return this.http.get<ApiResult>(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`)
+    return this.http.get<ApiResult>(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`);
   }
 
   getDetalhesFilmes(id: string): Observable<MovieResult>{
-    return this.http.get<MovieResult>(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)
+    return this.http.get<MovieResult>(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
   }
 }
